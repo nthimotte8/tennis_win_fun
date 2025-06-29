3ea268570f90cf8a6216dd7620929203afd94355
nthimotte8 <thimottenicolas@gmail.com>
2025-06-29 23:40:30 +0200

    strat alembic

---
+++ tennis_win_fun/build_historic/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Tournoi(Base):
+    __tablename__ = 'tournois'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tourney_id = Column(String, unique=True, nullable=True)  # nullable et unique
+    tourney_name = Column(String, nullable=False)
+    surface = Column(String)
+    draw_size = Column(Integer)
+    tourney_level = Column(String)
+    tourney_date = Column(Date)
+    tourney_start_date = Column(Date)
+
+class Joueur(Base):
+    __tablename__ = 'joueurs'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False, unique=True)
+    hand = Column(String)
+    ht = Column(Integer)
+    ioc = Column(String)
